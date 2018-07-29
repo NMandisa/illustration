9ae2cbcc2b51314181de788ae6746bda2fc2fa4c
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2018-07-29 05:47:40 +0530

    Removing comment

--- src/main/java/za/co/technical/assessment/illustration/controller/IPLocationController.java
+++ src/main/java/za/co/technical/assessment/illustration/controller/IPLocationController.java
@@ -1,5 +1,13 @@
 package za.co.technical.assessment.illustration.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(allowedHeaders = {"*"}, origins = "*")
+@RestController
+@RequestMapping(value = "/iploc")
 public class IPLocationController {
+
 
 }
