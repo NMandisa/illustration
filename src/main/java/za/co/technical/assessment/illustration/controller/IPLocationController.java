50bfe5ab95897c5ab856aa2da7af059e3818878c
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2018-07-29 07:45:34 +0530

    Controller Changes

--- src/main/java/za/co/technical/assessment/illustration/controller/IPLocationController.java
+++ src/main/java/za/co/technical/assessment/illustration/controller/IPLocationController.java
@@ -1,13 +1,25 @@
 package za.co.technical.assessment.illustration.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.co.technical.assessment.illustration.dto.ServerLocation;
+import za.co.technical.assessment.illustration.service.IPLocationService;
+
+import java.io.IOException;
 
 @CrossOrigin(allowedHeaders = {"*"}, origins = "*")
 @RestController
 @RequestMapping(value = "/iploc")
 public class IPLocationController {
+
+    @Autowired
+    IPLocationService ipLocationService;
+
+    @RequestMapping(value = "/{ip}", method = RequestMethod.POST,headers="Accept=application/json")
+    public ServerLocation iplocation (@PathVariable("ip") String ipAddress) throws IOException {
+        ServerLocation location = ipLocationService.getLocation(ipAddress);
+        return location;
+    }
 
 
 }
